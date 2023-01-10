063bda96db7c510a928791e2f26ca947a573565a
Yassine EL GHALLALI <y.elghallali@gmail.com>
2023-01-11 00:00:06 +0100

    Update exercice1.py

--- TD2/exercice1.py
+++ TD2/exercice1.py
@@ -11,6 +11,7 @@ class Book:
 b1 = Book("The Psychology of Money: Timeless lessons on wealth, greed, and happiness","Morgan Housel",12.2)
 b2 = Book("Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones","James Clear",14.11)
 
+print("------------------------------------")
 b1.view()
 print("------------------------------------")
 b2.view()
