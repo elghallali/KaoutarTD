b3c3dba56ea1151ece3d1c346191c21a80479428
Yassine EL GHALLALI <35077623+elghallali@users.noreply.github.com>
2023-01-10 23:50:33 +0100

    Update exercice1.py

---
+++ TD2/exercice1.py
@@ -0,0 +1,15 @@
+class Book:
+  def __init__(self, title, author, price):
+    self.title = title
+    self.author = author
+    self.price = price
+    
+  def view(self):
+    print(f"title : {self.title}, author : {self.author}, price : {self.price} $")
+
+b1 = Book("The Psychology of Money: Timeless lessons on wealth, greed, and happiness","Morgan Housel",12.2)
+b2 = Book("Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones","James Clear",14.11)
+
+b1.view()
+print("------------------------------------")
+b2.view()
