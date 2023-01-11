759755c5c43413ea08d8a5bbabb9267473b9b88c
Yassine EL GHALLALI <y.elghallali@gmail.com>
2023-01-11 01:19:51 +0100

    Update exercice2.py

---
+++ TD2/exercice2.py
@@ -0,0 +1,21 @@
+class Student:
+    def __init__(self, name, rollNumber, marks):
+        self.name = name
+        self.rollNumber = rollNumber
+        self.marks = marks
+
+    def accept(self):
+        pass
+
+    def display(self):
+        pass
+
+    def search(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def update(self):
+        pass
+
